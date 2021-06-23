55513dcef4b916f6c403bc9fd7e4ba8de533a511
Manivannan K <manivannan23k@gmail.com>
2021-06-23 18:57:33 +0530

    docs update 6

--- docs/conf.py
+++ docs/conf.py
@@ -45,7 +45,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 #html_theme = 'alabaster'
-html_theme = "sizzle"
+html_theme = "sphinx_book_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
